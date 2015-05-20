fe1d5f6f76e7b1f23d6550f9df922b1be052e7b5
Denis Kolobnev <denis.kolobnev@alpari.org>
2015-05-20 19:08:38 +0300

    file fix

--- templates/iface.rb
+++ templates/iface.rb
@@ -1,21 +1,21 @@
 auto <%= @iface_name %>
 iface <%= @iface_name %> inet <%= @iface_method %>
-<% if @iface_br_ports %>    bridge_ports <%= @iface_br_ports %>
+<% if @iface_br_ports %>\tbridge_ports <%= @iface_br_ports %>
 <% end -%>
-<% if @iface_br_fd %>     bridge_fd <%= @iface_br_fd %>
+<% if @iface_br_fd %>\tbridge_fd <%= @iface_br_fd %>
 <% end -%>
-<% if @iface_br_maxwait %>    bridge_maxwait <%= @iface_br_maxwait %>
+<% if @iface_br_maxwait %>\tbridge_maxwait <%= @iface_br_maxwait %>
 <% end -%>
-<% if @iface_hwaddr %>    hwaddrress ether <%= @iface_hwaddr %>
+<% if @iface_hwaddr %>\thwaddrress ether <%= @iface_hwaddr %>
 <% end -%>
-<% if @iface_addr %>    address <%= @iface_addr %>
+<% if @iface_addr %>\taddress <%= @iface_addr %>
 <% end -%>
-<% if @iface_netmask %>    netmask <%= @iface_netmask %>
+<% if @iface_netmask %>\tnetmask <%= @iface_netmask %>
 <% end -%>
-<% if @iface_gateway %>    gateway <%= @iface_gateway %>
+<% if @iface_gateway %>\tgateway <%= @iface_gateway %>
 <% end -%>
-<% if @iface_dns %>    dns-nameserver <%= @iface_dns %>
+<% if @iface_dns %>\tdns-nameserver <%= @iface_dns %>
 <% end -%>
-<% if @iface_dns_search %>    dns-search <%= @iface_dns_search %>
+<% if @iface_dns_search %>\tdns-search <%= @iface_dns_search %>
 <% end -%>
 
