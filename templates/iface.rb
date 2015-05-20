3d5cc5cd2a950a31c84001ed515ea509378bb25a
Denis Kolobnev <denis.kolobnev@alpari.org>
2015-05-20 18:56:47 +0300

    fix path

--- templates/iface.rb
+++ templates/iface.rb
@@ -1,5 +1,11 @@
 auto <%= @iface_name %>
 iface <%= @iface_name %> inet <%= @iface_method %>
+<% if @iface_br_ports %>    bridge_ports <%= @iface_br_ports %>
+<% end -%>
+<% if @iface_br_fd %>     bridge_fd <%= @iface_br_fd %>
+<% end -%>
+<% if @iface_br_maxwait %>    bridge_maxwait <%= @iface_br_maxwait %>
+<% end -%>
 <% if @iface_hwaddr %>    hwaddrress ether <%= @iface_hwaddr %>
 <% end -%>
 <% if @iface_addr %>    address <%= @iface_addr %>
