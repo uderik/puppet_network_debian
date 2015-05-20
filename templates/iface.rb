3754e433a52e859c93671c7f9e001b313b646ce4
Denis Kolobnev <denis.kolobnev@alpari.org>
2015-05-20 18:54:42 +0300

    fix path

--- templates/iface.rb
+++ templates/iface.rb
@@ -1,21 +1,15 @@
 auto <%= @iface_name %>
 iface <%= @iface_name %> inet <%= @iface_method %>
-#<% if !@iface_br_ports.empty? %>    bridge_ports <%= @iface_br_ports %>
-#<% end -%>
-#<% if !@iface_br_fd.empty? %>     bridge_fd <%= @iface_br_fd %>
-#<% end -%>
-#<% if !@iface_br_maxwait.empty? %>    bridge_maxwait <%= @iface_br_maxwait %>
-#<% end -%>
-#<% if !@iface_hwaddr.empty? %>    hwaddrress ether <%= @iface_hwaddr %>
+<% if @iface_hwaddr %>    hwaddrress ether <%= @iface_hwaddr %>
 <% end -%>
-<% if !@iface_addr.empty? %>    address <%= @iface_addr %>
+<% if @iface_addr %>    address <%= @iface_addr %>
 <% end -%>
-<% if !@iface_netmask.empty? %>    netmask <%= @iface_netmask %>
+<% if @iface_netmask %>    netmask <%= @iface_netmask %>
 <% end -%>
-<% if !@iface_gateway.empty? %>    gateway <%= @iface_gateway %>
+<% if @iface_gateway %>    gateway <%= @iface_gateway %>
 <% end -%>
-<% if !@iface_dns.empty? %>    dns-nameserver <%= @iface_dns %>
+<% if @iface_dns %>    dns-nameserver <%= @iface_dns %>
 <% end -%>
-<% if !@iface_dns_search.empty? %>    dns-search <%= @iface_dns_search %>
+<% if @iface_dns_search %>    dns-search <%= @iface_dns_search %>
 <% end -%>
 
